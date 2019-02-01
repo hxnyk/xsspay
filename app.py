30585091bf36e3f61f9e8d88ca30dbe6d7d7ffdd
d0nut <nathanial.lattimer@gmail.com>
2019-02-01 02:11:02 -0800

    Create static resource loaders

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-from flask import app, request
+from flask import app, render_template, request, send_from_directory
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="static")
+
+'''
+app.py
+static
+-- js
+-- -- script.js
+-- css
+-- -- style.css
+-- img
+-- -- icon.png
+templates
+-- index.html
+'''
+
+@app.route("/js/<path:path>")
+def load_js(path):
+    return send_from_directory("js", path)
+
+@app.route("/css/<path:path>")
+def load_css(path):
+    return send_from_directory("css", path)
+
+@app.route("/img/<path:path>")
+def load_img(path):
+    return send_from_directory("img", path)
+
+# -----------------------------------------
 
 @app.route("/")
 def index():
-    return "yeet"
+    return render_template('index.html', name=name)
