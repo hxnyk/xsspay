1f4f0f30e6cf4c3165c3a840dea249ceee662e6a
Hon Kwok <hon@sumologic.com>
2019-02-08 03:21:01 -0800

    add username and balance in templates

--- handlers/dashboard.py
+++ handlers/dashboard.py
@@ -1,18 +1,23 @@
 from flask import app, render_template
+import services
 import web
 
 @web.register("/dashboard")
-def getDashboard():
-    return render_template('dashboard.html')
+def getDashboard(auth):
+    context = services.user.get_user(auth['uid'])
+    return render_template('dashboard.html', **context)
 
 @web.register("/account")
-def getAccount():
-    return render_template('account.html')
+def getAccount(auth):
+    context = services.user.get_user(auth['uid'])
+    return render_template('account.html', **context)
 
 @web.register("/history")
-def getHistory():
-    return render_template('history.html')
+def getHistory(auth):
+    context = services.user.get_user(auth['uid'])
+    return render_template('history.html', **context)
 
 @web.register("/transactions")
-def getTransactions():
-    return render_template('transactions.html')
+def getTransactions(auth):
+    context = services.user.get_user(auth['uid'])
+    return render_template('transactions.html', **context)
